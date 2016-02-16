a975f96702e5687d76cb8c1dd301853a4af88f7a
linyongchao <lycwork@126.com>
2016-02-16 14:41:29 +0800

    add:增加github测试分支测试文件

---
+++ src/dev/DevTest.java
@@ -0,0 +1,13 @@
+package dev;
+
+/**
+ * This is a test for github 
+ * 为了验证强制推送，特地增加此文件
+ * 希望由feature分支强制替换dev的时候该分支不再保留
+ * 
+ * @author lin
+ * @date 2016年2月16日 下午2:38:23
+ */
+public class DevTest {
+
+}
